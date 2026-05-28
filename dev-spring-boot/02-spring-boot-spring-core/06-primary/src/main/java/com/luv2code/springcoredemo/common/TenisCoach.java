0536a7f3bb2ce438aa539cf5315f51acd4ff55ed
sbarza <sergiobarza@gmail.com>
2026-05-28 01:01:28 -0300

    Primary Annotation

---
+++ dev-spring-boot/02-spring-boot-spring-core/06-primary/src/main/java/com/luv2code/springcoredemo/common/TenisCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TenisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
