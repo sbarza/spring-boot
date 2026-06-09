be07a7c8d4a55c6f0c8945c00c82e615e2e62791
sbarza <sergiobarza@gmail.com>
2026-06-09 02:03:33 -0300

    Spring Boot REST: Spring Data REST

---
+++ dev-spring-boot/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // That's it ... no need to write any code LOL!
+}
